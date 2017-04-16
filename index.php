479b97cf259dfc2b0e14b4874511e4d4219bcbd1
hilario.code <hilario.code@gmail.com>
2017-04-16 11:39:26 -0300

    mudei o primeiro echo e adicionei outro

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-echo "Meu primeiro código PHP! <strong>Ola Mundo</strong>. Hilario.code";
+echo "Meu primeiro código PHP! <strong>Ola Mundo Vip</strong>. Hilario.code";
+
+echo "Ola Udemy"
 
 ?>
